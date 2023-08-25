3499e3b06d27079d1957785dc64b26a0b9e5f395
hyewone <won5854@naver.com>
2023-08-25 13:33:17 +0900

    apply select stageGreeting api

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,40 @@
 import { Helmet } from 'react-helmet-async';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { filter } from 'lodash';
+import { sentenceCase } from 'change-case';
 import { faker } from '@faker-js/faker';
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography, Card, CardHeader, Box } from '@mui/material';
+import {
+  Grid, 
+  Container, 
+  Typography, 
+  CardHeader, 
+  Box,
+  Card,
+  Table,
+  Stack,
+  Paper,
+  Avatar,
+  Button,
+  Popover,
+  Checkbox,
+  TableRow,
+  MenuItem,
+  TableBody,
+  TableCell,
+  IconButton,
+  TableContainer,
+  TablePagination,
+} from '@mui/material';
 
 // components
 import Iconify from '../components/iconify';
+import { BlogPostCard } from '../sections/@dashboard/blog';
+import { ProductList } from '../sections/@dashboard/products';
+import Label from '../components/label';
+import Scrollbar from '../components/scrollbar';
 // sections
 import {
   AppTasks,
@@ -20,76 +47,216 @@ import {
   AppCurrentSubject,
   AppConversionRates,
 } from '../sections/@dashboard/app';
-
-// components\
-import { BlogPostCard } from '../sections/@dashboard/blog';
-import { ProductList } from '../sections/@dashboard/products';
+import { UserListHead, UserListToolbar } from '../sections/@dashboard/user';
 
 // mock
 import POSTS from '../_mock/blog';
 import PRODUCTS from '../_mock/products';
+import sgList from '../_mock/user';
 
+// ----------------------------------------------------------------------
+
+
+const TABLE_HEAD = [
+  { id: 'CinemaType', label: '시네마', alignRight: false },
+  { id: 'company', label: '영화이름', alignRight: false },
+  { id: 't', label: '상영극장', alignRight: false },
+  { id: 'isVerified', label: '상영일자', alignRight: false },
+  { id: 'status', label: '상영시간', alignRight: false },
+  { id: 'attende', label: '참석자', alignRight: false },
+  { id: 'attende2', label: '예매하기', alignRight: false },
+];
+
+// ----------------------------------------------------------------------
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function applySortFilter(array, comparator, query) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  if (query) {
+    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  }
+  return stabilizedThis.map((el) => el[0]);
+}
 
 // ----------------------------------------------------------------------
 
 export default function HomePage() {
+  const apiUrl = "http://localhost:8080"
+
   const theme = useTheme();
 
-  const [megaboxSgUrlList, setMegaboxSgUrlList] = useState([]); // 뉴스 데이터 상태 관리
-  const [lotteSgUrlList, setLotteSgUrlList] = useState([]); // 주문 데이터 상태 관리
-  const [cgvSgUrlList, setCgvSgUrlList] = useState([]); // 주문 데이터 상태 관리
+  const [sgList, setSgList] = useState([]);                     // 무대인사
+  const [megaboxSgUrlList, setMegaboxSgUrlList] = useState([]); // 메가박스 무대인사 URL
+  const [lotteSgUrlList, setLotteSgUrlList] = useState([]);     // 롯데시네마 무대인사 URL
+  const [cgvSgUrlList, setCgvSgUrlList] = useState([]);         // CGV 무대인사 URL
 
+  const getApiUrl = (request) => {
+    return apiUrl + request
+  }
 
   const fetchAllData = async () => {
+    fetchSgList()
     fetchMegaboxSgUrlList()
-    fetchLotteStageGreetingList()
-    fetchCgvStageGreetingList()
+    fetchLotteSgUrlList()
+    fetchCgvSgUrlList()
   }
 
   // API 호출 함수 정의
+
+  const fetchSgList = async () => {
+    try {
+      const response = await axios.get(getApiUrl('/api/v1/stageGreetings'));
+      let data = response.data.stageGreetings
+      data = data == null ? [] : data
+      setSgList(data);
+    } catch (error) {
+      console.error('Error fetching stageGreetings:', error);
+    }
+  };
+
   const fetchMegaboxSgUrlList = async () => {
     try {
-      const response = await axios.get('http://localhost:8080/api/v1/stageGreetings?cinemaType=MEGABOX');
+      const response = await axios.get(getApiUrl('/api/v1/stageGreetingUrls?cinemaType=MEGABOX'));
       let data = response.data.stageGreetingUrls
       data = data == null ? [] : data
       setMegaboxSgUrlList(data);
     } catch (error) {
-      console.error('Error fetching megebox stageGreeting:', error);
+      console.error('Error fetching megebox stageGreetingUrls:', error);
     }
   };
 
-  const fetchLotteStageGreetingList = async () => {
+  const fetchLotteSgUrlList = async () => {
     try {
-      const response = await axios.get('http://localhost:8080/api/v1/stageGreetings?cinemaType=LOTTECINEMA');
+      const response = await axios.get(getApiUrl('/api/v1/stageGreetingUrls?cinemaType=LOTTECINEMA'));
       let data = response.data.stageGreetingUrls
       data = data == null ? [] : data
       setLotteSgUrlList(data);
     } catch (error) {
-      console.error('Error fetching orders:', error);
+      console.error('Error fetching lottecinema stageGreetingUrls:', error);
     }
   };
 
-  const fetchCgvStageGreetingList = async () => {
+  const fetchCgvSgUrlList = async () => {
     try {
-      const response = await axios.get('http://localhost:8080/api/v1/stageGreetings?cinemaType=CGV');
+      const response = await axios.get(getApiUrl('/api/v1/stageGreetingUrls?cinemaType=CGV'));
       let data = response.data.stageGreetingUrls
       data = data == null ? [] : data
       setCgvSgUrlList(data);
     } catch (error) {
-      console.error('Error fetching orders:', error);
+      console.error('Error fetching cgv stageGreetingUrls:', error);
     }
   };
+
+// ----------------------------------------------------------------------
+
+  const [open, setOpen] = useState(null);
+
+  const [page, setPage] = useState(0);
+
+  const [order, setOrder] = useState('asc');
+
+  const [selected, setSelected] = useState([]);
+
+  const [orderBy, setOrderBy] = useState('name');
+
+  const [filterName, setFilterName] = useState('');
+
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleOpenMenu = (event) => {
+    setOpen(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setOpen(null);
+  };
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = sgList.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setPage(0);
+    setRowsPerPage(parseInt(event.target.value, 10));
+  };
+
+  const handleFilterByName = (event) => {
+    setPage(0);
+    setFilterName(event.target.value);
+  };
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - sgList.length) : 0;
+
+  const filteredUsers = applySortFilter(sgList, getComparator(order, orderBy), filterName);
+
+  const isNotFound = !filteredUsers.length && !!filterName;
+
+
+// ----------------------------------------------------------------------
 
   useEffect(() => {
     fetchAllData()
   }, []);
 
   useEffect(() => {
-    console.log(megaboxSgUrlList)
-    console.log(lotteSgUrlList)
-    console.log(cgvSgUrlList)
+    console.log(sgList)
+    // console.log(megaboxSgUrlList)
+    // console.log(lotteSgUrlList)
+    // console.log(cgvSgUrlList)
   }, [megaboxSgUrlList, lotteSgUrlList, cgvSgUrlList]);
 
+
+// ----------------------------------------------------------------------
 
   return (
     <>
@@ -100,31 +267,178 @@ export default function HomePage() {
       <Container maxWidth="xl">
         <Typography variant="h4" sx={{ mb: 5 }} align="center"/>
        
-        <Grid container spacing={3}>
+        <Grid container spacing={10}>
 
+        <Grid item xs={12} md={12} lg={12}>
+
+          <Container>
+          <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5} ml={1}>
+            <Typography variant="h4" gutterBottom lm={1}>
+              무대인사 모아보기
+            </Typography>
+            <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>
+              상세 모아보기
+            </Button>
+          </Stack>
+
+          <Card>
+            {/* <sgListToolbar numSelected={selected.length} filterName={filterName} onFilterName={handleFilterByName} /> */}
+
+            <Scrollbar>
+              <TableContainer sx={{ minWidth: 800 }}>
+                <Table>
+                  <UserListHead
+                    order={order}
+                    orderBy={orderBy}
+                    headLabel={TABLE_HEAD}
+                    rowCount={sgList.length}
+                    // numSelected={selected.length}
+                    onRequestSort={handleRequestSort}
+                    // onSelectAllClick={handleSelectAllClick}
+                  />
+                  <TableBody>
+                    {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                      const { ID, CinemaType, Theater, ShowDate, ShowTime, AttendeeName } = row;
+                      const MovieName = row.Movie.Name;
+                      // const selectedUser = selected.indexOf(ID) !== -1;
+
+                      return (
+                        <TableRow hover key={ID} tabIndex={-1} role="checkbox">
+                          {/* <TableCell padding="checkbox">
+                            <Checkbox checked={selectedUser} onChange={(event) => handleClick(event, name)} />
+                          </TableCell> */}
+
+                          <TableCell align="center" component="th" scope="row" padding="none">
+                            {/* <Stack direction="row" alignItems="center" spacing={2}> */}
+                            <Avatar alt={CinemaType} src="" sx={{ margin: 'auto', display: 'block' }} />
+                              {/* <Typography variant="subtitle2" noWrap>
+                                {name}
+                              </Typography> */}
+                            {/* </Stack> */}
+                          </TableCell>
+
+                          <TableCell align="center">
+                            <Avatar alt={CinemaType} src="" sx={{ margin: 'auto', display: 'block' }} />
+                            {MovieName}
+                            </TableCell>
+
+                          <TableCell align="center">{Theater}</TableCell>
+
+                          <TableCell align="center">{ShowDate}</TableCell>
+
+                          <TableCell align="center">
+                            <Label color={(ShowTime === 'banned' && 'error') || 'success'}>{sentenceCase(ShowTime)}</Label>
+                          </TableCell>
+
+                          <TableCell align="center">
+                            {AttendeeName}
+                            {/* <IconButton size="large" color="inherit" onClick={handleOpenMenu}> */}
+                              {/* <Iconify icon={'eva:more-vertical-fill'} /> */}
+                            {/* </IconButton> */}
+                          </TableCell>
+                          <TableCell align="center">
+                            <Label color={(ShowTime === 'banned' && 'error') || 'success'}>{sentenceCase("예매하기")}</Label>
+                            {/* <IconButton size="large" color="inherit" onClick={handleOpenMenu}>
+                              <Iconify icon={'eva:more-vertical-fill'} /> 
+                            </IconButton> */}
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                    {emptyRows > 0 && (
+                      <TableRow style={{ height: 53 * emptyRows }}>
+                        <TableCell colSpan={6} />
+                      </TableRow>
+                    )}
+                  </TableBody>
+
+                  {isNotFound && (
+                    <TableBody>
+                      <TableRow>
+                        <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                          <Paper
+                            sx={{
+                              textAlign: 'center',
+                            }}
+                          >
+                            <Typography variant="h6" paragraph>
+                              Not found
+                            </Typography>
+
+                            <Typography variant="body2">
+                              No results found for &nbsp;
+                              <strong>&quot;{filterName}&quot;</strong>.
+                              <br /> Try checking for typos or using complete words.
+                            </Typography>
+                          </Paper>
+                        </TableCell>
+                      </TableRow>
+                    </TableBody>
+                  )}
+                </Table>
+              </TableContainer>
+            </Scrollbar>
+
+            {/* <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component="div"
+              count={sgList.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            /> */}
+          </Card>
+        </Container>
+      </Grid>
+          
           <Grid item xs={12} md={12} lg={12}>
-            <Card>
-              <CardHeader title="메가박스" subheader="" />
-              <Box sx={{ p: 3, pb: 1 }} dir="ltr">
-                <ProductList products={megaboxSgUrlList} />
-              </Box>
-              <CardHeader />
-            </Card>
+            <Container>
+              <Stack direction="row" alignItems="center" justifyContent="space-between" mb={2} ml={1}>
+                <Typography variant="h4" gutterBottom>
+                  무대인사 사이트 바로가기
+                </Typography>
+              </Stack>
+              <Card>
+                <CardHeader title="메가박스" subheader="" />
+                <Box sx={{ p: 3, pb: 1 }} dir="ltr">
+                  <ProductList products={megaboxSgUrlList} />
+                </Box>
+                <CardHeader />
+                <CardHeader title="롯데시네마" subheader="" />
+                <Box sx={{ p: 3, pb: 1 }} dir="ltr">
+                  <ProductList products={lotteSgUrlList} />
+                </Box>
+                {/* <Box sx={{ p: 3, pb: 1 }} dir="ltr">
+                  <Grid container spacing={3}>
+                    {POSTS.slice(0, 3).map((post, index) => (
+                      <BlogPostCard key={post.id} post={post} index={index} />
+                    ))}
+                  </Grid>
+                </Box> */}
+                <CardHeader />
+                <CardHeader title="CGV" subheader="" />
+                <Box sx={{ p: 3, pb: 1 }} dir="ltr">
+                  <ProductList products={cgvSgUrlList} />
+                </Box>
+                <CardHeader />
+              </Card>
+            </Container>
           </Grid>
 
-          <Grid item xs={12} md={12} lg={12}>
+          {/* <Grid item xs={12} md={12} lg={12}>
             <Card>
               <CardHeader title="롯데시네마" subheader="" />
               <Box sx={{ p: 3, pb: 1 }} dir="ltr">
                 <ProductList products={lotteSgUrlList} />
               </Box>
-              {/* <Box sx={{ p: 3, pb: 1 }} dir="ltr">
+              <Box sx={{ p: 3, pb: 1 }} dir="ltr">
                 <Grid container spacing={3}>
                   {POSTS.slice(0, 3).map((post, index) => (
                     <BlogPostCard key={post.id} post={post} index={index} />
                   ))}
                 </Grid>
-              </Box> */}
+              </Box>
               <CardHeader />
             </Card>
           </Grid>
@@ -137,7 +451,7 @@ export default function HomePage() {
               </Box>
               <CardHeader />
             </Card>
-          </Grid>
+          </Grid> */}
 
           <Grid item xs={12} sm={6} md={3}>
             <AppWidgetSummary title="프로필 관리" total={714000} icon={'ant-design:android-filled'} />
